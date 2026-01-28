2a21d0b541134cfda7019a89d018b9dab046baa3
Conner McCarthy <36969849+ConnerMcCarthy@users.noreply.github.com>
2026-01-28 18:41:54 -0500

    Delete .next/dev directory

--- .next/dev/server/pages/_app.js
+++
@@ -1,4 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_app.js")
-R.c("server/chunks/ssr/[root-of-the-server]__61605ffc._.js")
-R.m("[project]/conner-business-site/node_modules/next/app.js [ssr] (ecmascript)")
-module.exports=R.m("[project]/conner-business-site/node_modules/next/app.js [ssr] (ecmascript)").exports
--- .next/dev/server/pages/_document.js
+++
@@ -1,5 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_document.js")
-R.c("server/chunks/ssr/7a27e_5fcc8e9c._.js")
-R.c("server/chunks/ssr/[externals]__e6a4d965._.js")
-R.m("[project]/conner-business-site/node_modules/next/document.js [ssr] (ecmascript)")
-module.exports=R.m("[project]/conner-business-site/node_modules/next/document.js [ssr] (ecmascript)").exports
